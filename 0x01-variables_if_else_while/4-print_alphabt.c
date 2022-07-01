1ffb753581733e3aa7087f53cf21ea85183883e0
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-01 22:45:35 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-if (alphabet == 'q')
-continue;
-else if (alphabet == 'e')
-continue;
-putchar(alphabet);
-}
-putchar('\n');
-return (0);
+        char alphabet;
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+        if (alphabet == 'q')
+        continue;
+        else if (alphabet == 'e')
+        continue;
+        putchar(alphabet);
+        }
+        putchar('\n');
+        return (0);
 }
