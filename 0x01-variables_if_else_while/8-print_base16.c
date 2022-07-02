db30045fe64cd587b50320791be912715e5b3606
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-02 01:36:36 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-        char base_16;
-        for (base_16 = 48; base_16 <= 57; base_16++)
-        {putchar(base_16);
-        }
-        for (base_16 = 97 ; base_16 <= 102; base_16++)
-        {
-        putchar(base_16);
-        }
-        putchar ('\n');
-        return (0);
+char base_16;
+for (base_16 = 48; base_16 <= 57; base_16++)
+{putchar(base_16);
+}
+for (base_16 = 97 ; base_16 <= 102; base_16++)
+{
+putchar(base_16);
+}
+putchar ('\n');
+return (0);
 }
