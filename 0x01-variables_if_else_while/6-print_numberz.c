0440fbfa973391240687eb972048e14e83963df0
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-02 01:38:43 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-        int i;
-        for (i = 48 ; i <= 57; i++)
-        {
-        putchar (i);
-        }
-        putchar ('\n');
-        return (0);
+int i;
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
 }
