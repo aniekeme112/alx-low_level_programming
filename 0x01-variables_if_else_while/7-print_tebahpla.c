af3c963bd3c4962e9da498ee65fce3845cd73deb
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-02 01:37:51 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-        char alphabet;
-        for (alphabet = 122 ; alphabet >= 97; alphabet--)
-        {
-        putchar(alphabet);
-        }
-        putchar('\n');
-        return (0);
+char alphabet;
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
