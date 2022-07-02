932fe343c7440948870d1df5ff676ca346c2f7a0
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-02 01:39:56 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-        int i;
-        for (i = 0 ; i < 10; i++)
-        {
-        printf("%i", i);
-        }
-        printf("\n");
-        return (0);
+int i;
+for (i = 0 ; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
 }
