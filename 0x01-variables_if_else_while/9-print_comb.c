3da6f89deb840376209aa5ad49cf89766ff2ef3d
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2022-07-02 01:34:02 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,17 @@
 */
 int main(void)
 {
-        int number;
-        for (number = 48; number <= 57; number++)
-        {
-        putchar(number);
-        if (number == 57)
-        {
-        break;
-        }
-        putchar(',');
-        putchar (' ');
-        }
-        putchar('\n');
-        return (0);
+int number;
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar (' ');
+}
+putchar('\n');
+return (0);
 }
